4f2a2549f90f374fbeb48f0be45e6531345ed23f
Ben Snell <benjaminpaulsnell@gmail.com>
2018-01-21 00:09:04 -0700

    Completed 02.03

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -15,9 +15,13 @@
  */
 package com.example.android.datafrominternet.utilities;
 
+import android.net.Uri;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -45,8 +49,18 @@ public class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String githubSearchQuery) {
-        // TODO (1) Fill in this method to build the proper Github query URL
-        return null;
+        // DONE (1) Fill in this method to build the proper Github query URL
+        Uri.Builder gitHubUriBuilder = Uri.parse(GITHUB_BASE_URL).buildUpon();
+        gitHubUriBuilder.appendQueryParameter(PARAM_QUERY, githubSearchQuery);
+        gitHubUriBuilder.appendQueryParameter(PARAM_SORT, sortBy);
+        Uri gitHubUri = gitHubUriBuilder.build();
+        URL gitHubURL = null;
+        try{
+            gitHubURL = new URL(gitHubUri.toString());
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return gitHubURL;
     }
 
     /**
